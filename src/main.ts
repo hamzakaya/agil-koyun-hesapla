83db86a3fd70f8f61825d7fa1da31988f49ffab6
Hamza KAYA <hamza.kaya@icterra.com>
2021-10-14 19:04:09 +0300

    input default

--- src/main.ts
+++ src/main.ts
@@ -32,20 +32,20 @@ document.addEventListener("DOMContentLoaded", () => {
   document.title = "Ağıl Hesaplama";
   const form = document.querySelector("#form") as HTMLFormElement;
   const sonucDiv = document.querySelector("#sonuc") as HTMLDivElement;
-
+  
   form.addEventListener("submit", (e: SubmitEvent) => {
     e.preventDefault();
     const formData = new FormData(form);
-    
+
     // const data = Object.fromEntries(
     //   Array.from(formData.keys()).map((key) => [key, ~~formData.get(key)])
     // );
     // const agil = new AgilHesapla(data);
 
     const agil = new AgilHesapla({
-      adet: ~~formData.get("adet"),
-      kapasite: ~~formData.get("kapasite"),
-      koyunSayisi: ~~formData.get("koyunSayisi"),
+      adet: ~~formData.get("adet") || 0,
+      kapasite: ~~formData.get("kapasite") || 0,
+      koyunSayisi: ~~formData.get("koyunSayisi") || 0,
     });
 
     sonucDiv.innerHTML = sonucOlustur(agil);
